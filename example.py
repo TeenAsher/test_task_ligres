155e27cb3fc8883f9983c16aea138ea335418dde
TeenAsher <106741447+TeenAsher@users.noreply.github.com>
2023-02-08 10:18:54 -0600

    Final

---
+++ example.py
@@ -0,0 +1,5 @@
+from numbers_and_operators import numbers
+from numbers_and_operators import operations
+
+a = numbers.six(operations.divided_by(numbers.two()))    # 6 делить на 2, ожидаемый результат = 3.0
+print(a)    # Вывод результата 3.0
---
+++ numbers_and_operators/numbers.py
@@ -0,0 +1,50 @@
+"""Тестовое задание от компании ЛигРес на вакансию
+   Middle Python Developer.
+   От Шилиной Татьяной 08.02.2023.
+   Исходный код: пакет numbers_and_operations с двумя модулями,
+   numbers и operations,
+   содержащими два типа функций - "числа" и "операции",
+   которые работают путем наложения друг на друга,
+   вместе возвращают математическе операции с числами (прим. seven(times(five())).
+   МОДУЛЬ - numbers
+   """
+
+
+# Функция возвращает 0, если не определен аргумент в виде функции.
+def zero(func=None): return 0 if not func else func(0)
+
+
+# Функция возвращает 1, если не определен аргумент в виде функции.
+def one(func=None): return 1 if not func else func(1)
+
+
+# Функция возвращает 2, если не определен аргумент в виде функции.
+def two(func=None): return 2 if not func else func(2)
+
+
+# Функция возвращает 3, если не определен аргумент в виде функции.
+def three(func=None): return 3 if not func else func(3)
+
+
+# Функция возвращает 4, если не определен аргумент в виде функции.
+def four(func=None): return 4 if not func else func(4)
+
+
+# Функция возвращает 5, если не определен аргумент в виде функции.
+def five(func=None): return 5 if not func else func(5)
+
+
+# Функция возвращает 6, если не определен аргумент в виде функции.
+def six(func=None): return 6 if not func else func(6)
+
+
+# Функция возвращает 7, если не определен аргумент в виде функции.
+def seven(func=None): return 7 if not func else func(7)
+
+
+# Функция возвращает 8, если не определен аргумент в виде функции.
+def eight(func=None): return 8 if not func else func(8)
+
+
+# Функция возвращает 9, если не определен аргумент в виде функции.
+def nine(func=None): return 9 if not func else func(9)
